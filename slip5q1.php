12e5cfd3fe85d9a56e3836503bed05e4c22cf92f
Soham Vijay Kulkarni <sohamkul2005@gmail.com>
2025-04-06 16:20:49 +0530

    Update slip5q1.php

--- slip5q1.php
+++ slip5q1.php
@@ -1,22 +1,22 @@
 /^[a-z]{10}@gmail.com$/
 
 <?php
-function validatePAN($phone) {
-    // Regular expression for PAN format
+function validatemail($emailid) {
+   
     $pattern = "/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/";
     
-    if (preg_match($pattern, $phone)) {
-        return "✅ Valid Phone Number.";
+    if (preg_match($pattern, $emailid)) {
+        return "✅ Valid Email id.";
     } else {
-        return "❌ Invalid Phone Number.";
+        return "❌ Invalid email id.";
     }
 }
 
-// Test PAN Numbers
-$phoneno1 = "sohamkul2005@gmail.com"; // Valid
-$phoneno2 = "ABC1234EF4"; // Invalid
-$phoneno3 = "ABCD12345F"; // Invalid
 
-echo $phoneno1 . " → " . validatePAN($phoneno1) . PHP_EOL;
-echo $phoneno2 . " → " . validatePAN($phoneno2) . PHP_EOL;
-echo $phoneno3 . " → " . validatePAN($phoneno3) . PHP_EOL;
+$emailid1 = "sohamkul2005@gmail.com"; // Valid
+$emailid2 = "ABC1234EF4"; // Invalid
+$emailid3 = "ABCD12345F"; // Invalid
+
+echo $emailid1 . " → " . validatePAN($emailid1) . PHP_EOL;
+echo $emailid2 . " → " . validatePAN($emailid2) . PHP_EOL;
+echo $emailid3 . " → " . validatePAN($emailid13) . PHP_EOL;

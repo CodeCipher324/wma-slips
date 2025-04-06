fe956791c98303b328ec29c7260f990e2a8e9fc6
Soham Vijay Kulkarni <sohamkul2005@gmail.com>
2025-04-06 16:41:50 +0530

    Update slip5q1.php

--- slip5q1.php
+++ slip5q1.php
@@ -17,6 +17,6 @@ $emailid1 = "sohamkul2005@gmail.com"; // Valid
 $emailid2 = "ABC1234EF4"; // Invalid
 $emailid3 = "ABCD12345F"; // Invalid
 
-echo $emailid1 . " → " . validatePAN($emailid1) . PHP_EOL;
-echo $emailid2 . " → " . validatePAN($emailid2) . PHP_EOL;
-echo $emailid3 . " → " . validatePAN($emailid13) . PHP_EOL;
+echo $emailid1 . " → " . validateemail($emailid1) . PHP_EOL;
+echo $emailid2 . " → " . validateemail($emailid2) . PHP_EOL;
+echo $emailid3 . " → " . validateemail($emailid3) . PHP_EOL;
